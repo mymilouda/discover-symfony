172d6c0127dd4cc688a427912c1b4fce4803d948
mymilouda <mymilouda59@gmail.com>
2019-04-02 17:09:57 +0200

    -

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,15 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class WelcomeController
+class WelcomeController extends AbstraController
 {
     /**
      *  @Route("/hello", name="hello")
      */
     public function hello()
     {
+        $name = 'Matthieu';
+
+        dump($name);
+
+        return $this->render('welcome/hello.html.twig', [
+            'name' => $name
+        ]);
+
         return new Response('
-        <html><body>Hello Matthieu</body></html>');
+        <html><body> '.$name.'</body></html>'
+        );
     }
 }
